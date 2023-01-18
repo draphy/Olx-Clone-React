2bafa64ea70d753af63293a36dcbcc3045e50737
draphy <davidraphi@proton.me>
2023-01-18 19:42:20 +0530

    version 1.1

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,13 @@ import 'firebase/auth'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBtRSJ5WNJ1lih7C0-zcPpTAWUhCngr9uA",
-  authDomain: "olx-react-clone-9bedd.firebaseapp.com",
-  projectId: "olx-react-clone-9bedd",
-  storageBucket: "olx-react-clone-9bedd.appspot.com",
-  messagingSenderId: "445600921534",
-  appId: "1:445600921534:web:bba8ce344aad2ee76aebef",
-  measurementId: "G-RJB01LY96G"
+  apiKey: " ",
+  authDomain: " ",
+  projectId: " ",
+  storageBucket: " ",
+  messagingSenderId: " ",
+  appId: " ",
+  measurementId: " "
 };
 
  export default firebase.initializeApp(firebaseConfig)
